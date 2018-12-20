22ab3b8364f0303a9454ba7fb102a6d0b9ed92f4
jenchal <tovpinets.victor@gmail.com>
2018-12-20 17:46:40 +0200

    fix eslint mistake

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-function  isNumber(value) {
+function isNumber (value) {
 	return typeof value === 'number';
 }
 
-function getMaxValue(maxValue,currentValue) {
+function getMaxValue (maxValue, currentValue) {
 	return maxValue > currentValue ? maxValue : currentValue;
 }
 
-function getMinValue(minValue,currentValue) {
+function getMinValue (minValue, currentValue) {
 	return minValue < currentValue ? minValue : currentValue;
 }
 
-function getSum(sumValue, currentValue) {
+function getSum (sumValue, currentValue) {
 	return sumValue + currentValue;
 }
 
-export function min(arrNumbers[]) {
-	return arrNumbers;
-	.filter(isNumber);
-	.reduce(getMinValue,undefined);
+export function min (arrNumbers = []) {
+	return arrNumbers
+	.filter(isNumber)
+	.reduce(getMinValue, undefined);
 }
 
-export function max(arrNumbers[]) {
-	return arrNumbers;
-	.filter(isNumber);
-	.reduce(getMaxValue,undefined);
+export function max (arrNumbers = []) {
+	return arrNumbers
+	.filter(isNumber)
+	.reduce(getMaxValue, undefined);
 }
 
-export function sum(...arrNumbers) {
-	return arrNumbers;
-	.filter(isNumber);
-	.reduce(getSum,0);
+export function sum (...arrNumbers) {
+	return arrNumbers
+	.filter(isNumber)
+	.reduce(getSum, 0);
 }
